a2e9a70ae8dc1137b44c8cd92de9fa92a5d9535b
Natalya <natalya.rozikulyeva@ya.ru>
2022-11-24 01:46:41 +0300

    добавлен Docker

--- star/src/components/pages/starships-page.js
+++ star/src/components/pages/starships-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StarshipList } from '../sw-components';
-import { withRouter } from 'react-router-dom';
+import { withRouter } from '../hoc-helpers';
 
 const StarshipsPage = ({ history }) => {
   return (
